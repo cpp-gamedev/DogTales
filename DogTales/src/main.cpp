f9d0a35ed41da5ff45ed85933f653e3b08f8fdc1
Karn Kaul <karnkaul@gmail.com>
2024-01-28 12:14:56 +0530

    Add versioning and command line argument parsing.

--- DogTales/src/main.cpp
+++ DogTales/src/main.cpp
@@ -1,7 +1,32 @@
+#include <bave/clap/clap.hpp>
 #include <bave/desktop_app.hpp>
+#include <src/build_version.hpp>
 #include <src/dogtales.hpp>
+#include <iostream>
 
-int main(int argc, char** argv) {
+namespace {
+auto parse_args(int const argc, char const* const* argv) -> std::optional<int> {
+	namespace clap = bave::clap;
+	auto name = clap::make_app_name(*argv);
+	auto description = std::string{"2D platformer game using bave"};
+	auto version = bave::to_string(dog_tales::build_version_v);
+	auto options = clap::Options{std::move(name), std::move(description), std::move(version)};
+
+	auto show_bave_version = false;
+	options.flag(show_bave_version, "bave-version", "show bave version");
+
+	auto const result = options.parse(argc, argv);
+	if (clap::should_quit(result)) { return clap::return_code(result); }
+
+	if (show_bave_version) {
+		std::cout << "bave version " << bave::to_string(bave::build_version_v) << "\n";
+		return EXIT_SUCCESS;
+	}
+
+	return {};
+}
+
+auto run_app(int const argc, char const* const* argv) -> int {
 	auto create_info = bave::DesktopApp::CreateInfo{
 		.args = bave::make_args(argc, argv),
 		.title = "DogTales",
@@ -14,4 +39,11 @@ int main(int argc, char** argv) {
 	app.set_bootloader([](bave::App& app) { return std::make_unique<DogTales>(app); });
 
 	return static_cast<int>(app.run());
+}
+} // namespace
+
+int main(int argc, char** argv) {
+	if (auto const ret = parse_args(argc, argv)) { return *ret; }
+
+	return run_app(argc, argv);
 }
